7c41c24c596438718974d5055686f90fe55fcd8d
adarshsingh1407 <adarshsingh1407@gmail.com>
2018-11-02 18:13:50 +0530

    body parser added

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -3,6 +3,14 @@ const SERVER_CONFIG = {
   MORGAN_FORMAT: '(STATUS~:status) ":method :url HTTP/:http-version" (REM_ADDR~:remote-addr) (RES_TIME~:response-time[3]) (REM_USER~:remote-user) (RES_CON_LENGTH~:res[content-length]) (REFERRER~:referrer) (USER_AGENT~:user-agent)',
   COMPRESSION_OPTIONS: {
     level: 1
+  },
+  BODY_PARSER_OPTIONS: {
+    JSON: {
+      limit: '500mb'
+    },
+    URL_ENCODED: {
+      extended: false
+    }
   }
 }
 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const compression = require('compression');
 const morgan = require('morgan');
+const bodyParser = require('body-parser');
 const SERVER_CONFIG = require('./config/serverConfig');
 
 const port = process.env.PORT || SERVER_CONFIG.DEFAULT_PORT;
@@ -14,8 +15,21 @@ app.use(morgan(SERVER_CONFIG.MORGAN_FORMAT));
 // Compression
 app.use(compression(SERVER_CONFIG.COMPRESSION_OPTIONS))
 
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded(SERVER_CONFIG.BODY_PARSER_OPTIONS.URL_ENCODED))
+
+// parse application/json
+app.use(bodyParser.json(SERVER_CONFIG.BODY_PARSER_OPTIONS.JSON))
+
 app.get('/', (req, res) => {
   res.send('CacheApp Online!');
+});
+
+app.post('/', (req, res) => {
+  res.send({
+    message: 'CacheApp Online!',
+    data: req.body
+  });
 });
 
 app.listen(port, () => console.log(`CacheApp Online on port:${port} with env:${nodeEnv}`));
